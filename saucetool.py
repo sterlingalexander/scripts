dbb57059c34a3f69935c3fd171bb8c79aa7d7f77
Sterling Alexander <admin@five13.io>
2020-01-03 19:45:47 -0500

    Adjusted regex and added sysctl compare, it's rough but it's in.

--- saucetool.py
+++ saucetool.py
@@ -26,6 +26,8 @@ from collections import defaultdict
 # Functions
 #################################################
 
+# ==========| Data structure methods/helpers
+
 # Set up a factory
 def tree():
     """
@@ -37,10 +39,38 @@ def tree():
 # Put a list into the tree, creating any keys that don't exist
 def insert(tree, tokens):
     for t in tokens:
+        if t in excluded:
+            continue
         tree = tree[t]
 
 
-def rpm_name_decompose(s, unsplit, hostname):
+# ==========| Regex functions
+
+def split_with_regex(s, regex, errlog, hostname):
+    """
+    Generic function to split strings into a list with the passed in regular expression
+    :param s:  String to split
+    :param regex:  Regex to use
+    :param errlog:  Record any unsplit strings
+    :param hostname:  Host that provided the string
+    :return:  List of values from the string
+    """
+
+    result = filter(None, re.split(regex, s))
+    # If the length of the resulting list is 1, the name was not split.  Record it and
+    #   do not add the RPM to the compare tree
+    if len(result) == 1:
+        if hostname in errlog.keys():
+            errlog[hostname].append(result[0])
+        else:
+            print result, hostname
+            errlog[hostname] = []
+            errlog[hostname].append(result[0])
+        return None
+    return result
+
+
+def rpm_name_decompose(s, errlog, hostname):
     """
     This will split the RPM (hopefully) into the 'name' component, the full version 
     info, and the release and arch info.  Result returned as a list.
@@ -54,33 +84,52 @@ def rpm_name_decompose(s, unsplit, hostname):
     # If the length of the resulting list is 1, the name was not split.  Record it and
     #   do not add the RPM to the compare tree
     if len(result) == 1:
-        if hostname in unsplit.keys():
-            unsplit[hostname].append(result[0])
+        if hostname in errlog.keys():
+            errlog[hostname].append(result[0])
         else:
-            unsplit[hostname] = []
-            unsplit[hostname].append(result[0])
+            errlog[hostname] = []
+            errlog[hostname].append(result[0])
         return None
     return result
 
 
-def parse_rpms_to_tree(tree, rpmfile, hostname):
+# ==========| Parse into data structures
+
+def parse_data_to_tree(tree, regex, errlog, data_file, hostname):
     """
     Fills a passed in dictionary, using RPM names split at each '-'.  Hostnames are
     put as leaf values
     """
-    for line in rpmfile.readlines():
+    for line in data_file.readlines():
         # Send the RPM name to be split without the date
-        rpm_tokens = rpm_name_decompose(line.split()[0], unsplit, hostname)
+        tokens = split_with_regex(line.strip(), regex, errlog, hostname)
         # Append hostname before insertion
         try:
-            rpm_tokens.append(hostname)
+            tokens.append(hostname)
             # Add each token on the list to the tree
-            insert(tree, rpm_tokens)
+            insert(tree, tokens)
         # Here we only want to avoid a traceback for an unhandled RPM name.  We have
         #   already recorded the offending RPM in a list.
         except AttributeError:
             continue
 
+
+# ==========| Generic helpers
+
+def get_hostname(sos_path):
+    return open(sos_path + '/hostname', 'r').read().strip() + '-' + \
+         ''.join(open(sos_path + '/date', 'r').read().strip()[4:19].replace(' ', '-'))
+
+
+def print_tree(data_tree):
+    """
+    Any 'tree' data structure can be visualized with JSON using the function
+    """
+    import json
+    print json.dumps(data_tree, sort_keys=True, indent=2)
+
+
+# ==========| Argparse setup
 
 # Options for argument parsing
 def add_arguments(opts):
@@ -96,40 +145,57 @@ def add_arguments(opts):
 # Definitions
 #################################################
 
-rpm_compare_tree = tree()
-hosts = []
+# Base data
 opts = argparse.ArgumentParser()
-unsplit = {}
+hosts = []
+
+# Tokens to exclude from tree addition (prevents nexting issues, can probably use regular expressing to consume them
+excluded = ['=', ]
+
+# Data for RPM operations
+rpm_compare_tree = tree()
+rpm_split_errlog = {}
+
+# Data for sysctl operation
+sysctl_compare_tree = tree()
+sysctl_split_errlog = {}
 
 
 #################################################
+# Regular expressions
+#################################################
+
+"""
+This will split the RPM (hopefully) into the 'name' component, the full version 
+info, and the release and arch info.  Result returned as a list.
+>> Note the outer parenthesis, they are necessary to keep re.split from
+consuming the separator (version info in this case).
+"""
+rpm_split_regex = r'-([\d\-.]+(?:git|svn|git[\w\d]+|cvs)?[\d\-.]+)(el\d_?\d?.*|(?:el\d_?\d?|rhel\d)?.x86_64|(?:el\d)?.noarch|.\(none\)|i386)'
+rpm_split_regex2 = '-([\d\-.]+(?:git|svn|git[\w\d]+|cvs)?[\d\-.]+)(el\d_?\d{,3}.?\d?.[\w]+|(?:el\d_?\d?|rhel\d)?.x86_64|(?:el\d)?.noarch|.\(none\)|i386)'
+sysctl_split_regex = r'(=)'
+
+#################################################
 # Main
-################################################
+#################################################
 
 add_arguments(opts)
 run_opts = opts.parse_args()
+sos_targets = run_opts.path[0]
 
 # Make sure we got at least 2 sosreports on the command line
 if len(run_opts.path[0]) <= 1:
     print("\n\tOnly one path supplied, please specify a path to 2 sosreport directories.\n\n")
     sys.exit(1)
 
-
-# Populate a list of hosts from the passed in sosreport directories and parse the
-#  RPM data into the tree.  Paths are in run_opts.path[0] as a list.
-#
-# TODO: There is really no error checking, so if the files are missing just complain
-#         as long as there are at least 2 valid hosts provided
-for path in run_opts.path[0]:
+# Get all the hostnames
+for sos in sos_targets:
     try:
-        hn = open(path + '/hostname', 'r').read().strip() + '-' + \
-                ''.join(open(path + '/date', 'r').read().strip()[4:19].replace(' ','-'))
-        f  = open(path + '/installed-rpms', 'r') 
+        hn = get_hostname(sos)
         hosts.append(hn)
-        parse_rpms_to_tree(rpm_compare_tree, f, hn)
     except IOError:
         print("\n\tError attempting access to files in the following provided path:\n\t\t%s\n\tAttempting to "
-              "continue...\n" % path)
+              "continue...\n" % sos)
 
 # If we weren't able to read/parse more than one unique hostname we should stop
 if len(set(hosts)) <= 1:
@@ -137,17 +203,31 @@ if len(set(hosts)) <= 1:
     print ("\t2 unique sosreports are required to compare.\n")
     sys.exit(1)
 
+# Parse the RPM data into the tree.  Paths are in sos_targets as a list.
+#   This (or any other tree) can be visualized with the 'print_tree" helper function
+#
+# TODO: There is really no error checking, so if the files are missing just complain
+#         as long as there are at least 2 valid hosts provided
+for sos_path in sos_targets:
+    try:
+        hn = get_hostname(sos_path)
+        f = open(sos_path + '/installed-rpms', 'r')
+        parse_data_to_tree(rpm_compare_tree, rpm_split_regex2, rpm_split_errlog, f, hn)
+    except IOError:
+        print("\n\tError attempting access to files in the following provided path:\n\t\t%s\n\tAttempting to "
+              "continue...\n" % sos_path)
+
 # Count all the RPMs that could not be parsed by the regex
 unique_set = []
-for host in unsplit:
-    unique_set += unsplit[host]
+for host in rpm_split_errlog:
+    unique_set += rpm_split_errlog[host]
 
 # List all unparsed RPMs by host
 print("\nTotal number unique RPMs that the regex was unable to handle:\t%d" % len(set(unique_set)))
 print("List of unparsed RPMs by host.  IMPORTANT!:  These RPMs were not part of the comparison:\n")
-for host in unsplit:
+for host in rpm_split_errlog:
     print("%s:" % host)
-    for rpm in unsplit[host]:
+    for rpm in rpm_split_errlog[host]:
         print("\t%s" % rpm)
     print('\n')
 
@@ -166,8 +246,44 @@ for rpm in sorted(rpm_compare_tree):
                 for host in rpm_compare_tree[rpm][version][relarch]:
                     output += "\t\t%s\n" % host
     if flag:
-        print output    
+        print output
 
-# The tree data structure can be visualized by uncommenting the following.
-#import json
-#print json.dumps(rpm_compare_tree, sort_keys=True, indent=2)
+#########################################################################
+
+# Parse the sysctl data into the tree.  Paths are in sos_targets as a list.
+#
+# TODO: There is really no error checking, so if the files are missing just complain
+#         as long as there are at least 2 valid hosts provided
+for sos_path in sos_targets:
+    try:
+        hn = get_hostname(sos_path)
+        f = open(sos_path + '/sos_commands/kernel/sysctl_-a', 'r')
+        parse_data_to_tree(sysctl_compare_tree, sysctl_split_regex, sysctl_split_errlog, f, hn)
+    except IOError:
+        print("\n\tError attempting access to files in the following provided path:\n\t\t%s\n\tAttempting to "
+              "continue...\n" % sos_path)
+
+# Count all the RPMs that could not be parsed by the regex
+unique_set = []
+for host in sysctl_split_errlog:
+    unique_set += sysctl_split_errlog[host]
+
+# List all unparsed RPMs by host
+print("\nTotal number unique sysctl values the regex was unable to handle:\t%d" % len(set(unique_set)))
+print("List of unparsed sysctl values by host.  IMPORTANT!:  These values were not part of the comparison:\n")
+#for host in sysctl_split_errlog:
+#    print("%s:" % host)
+#    for sysctl in sysctl_split_errlog[host]:
+#        print("\t%s" % sysctl)
+#    print('\n')
+
+print("\nNow listing all differences:\n")
+
+print_tree(sysctl_compare_tree)
+
+for sysctl in sysctl_compare_tree:
+    for value in sysctl_compare_tree[sysctl]:
+        if len(sysctl_compare_tree[sysctl][value]) != len(hosts):
+            print sysctl, sysctl_compare_tree[sysctl][value]
+
+
